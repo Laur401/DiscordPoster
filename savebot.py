ca017a4c2231674a6d20e36130146a08861333a6
Your Name <183149493+Laur401@users.noreply.github.com>
2024-09-29 13:17:42 +0200

    Fixed problem with YAML reading.

--- savebot.py
+++ savebot.py
@@ -44,7 +44,7 @@ class FileManager:
 
     def read_file(self):
         f = open(f"{self.file}.yaml", "r")
-        return yaml.safe_load_all(f)
+        return list(yaml.safe_load_all(f))
 
 class KeyWorker:
     def __init__(self,dics):
@@ -52,6 +52,7 @@ class KeyWorker:
 
     def key_lister(self,target_key):
         i=1
+        print(self.dics)
         for dic in self.dics:
             for key, value in dic.items():
                 if key == target_key:
@@ -60,30 +61,34 @@ class KeyWorker:
 
     def key_getter(self,target_key,i):
         values=[]
+        print(self.dics)
         for dic in self.dics:
+            print(f"This is a dic: {dic}")
             for key, value in dic.items():
                 if key == target_key:
                     values.append(value)
-        if 1<=i<=len(values):
-            return values[i-1]
-        else:
-            return None
+        #if 1<=i<=len(values):
+        print(values)
+        return values[i-1]
+        #else:
+            #return None
 
 def send_message(users,channels,messages):
     api_version=6
     users_key=KeyWorker(users)
     channels_key=KeyWorker(channels)
     messages_key=KeyWorker(messages)
-    users_key.key_lister(users_key)
+    users_key.key_lister("user_id")
     user_nr=int(input("Which user to use?: "))
-    channels_key.key_lister(channels_key)
+    channels_key.key_lister("channel_id")
     channel_nr=int(input("Which channel to use?: "))
-    messages_key.key_lister(messages_key)
+    messages_key.key_lister("message")
     message_nr=int(input("Which message to send?: "))
     user_id=users_key.key_getter("user_id",user_nr)
     user_token=users_key.key_getter("user_token",user_nr)
     channel_url=channels_key.key_getter("channel_url",channel_nr)
     channel_id=channels_key.key_getter("channel_id",channel_nr)
+    message=messages_key.key_getter("message",message_nr)
     header_data={
         "content-type":"application/json",
         "user-id":user_id,
@@ -91,8 +96,9 @@ def send_message(users,channels,messages):
         "host":"discordapp.com",
         "referrer":channel_url
     }
-    message_data=json.dumps({"content":messages[message_nr].message})
+    message_data=json.dumps({"content":message})
     conn=HTTPSConnection("discordapp.com",443)
+    print(header_data)
     conn.request("POST",f"/api/v{api_version}/channels/{channel_id}/messages", message_data, header_data)
     print(conn.getresponse())
 
@@ -101,8 +107,6 @@ def main():
         user_input()
 
 def user_input():
-
-
     i=input("Enter 1 to add a new user.\nEnter 2 to add a new channel.\nEnter 3 to add a new message.\nEnter 4 to post a message.: ")
     if i=="1":
         user_entry()
@@ -111,30 +115,30 @@ def user_input():
     if i=="3":
         message_entry()
     if i=="4":
-        users_file = FileManager("users.json")
+        users_file = FileManager("users")
         users = users_file.read_file()
-        channels_file = FileManager("channels.json")
+        channels_file = FileManager("channels")
         channels = channels_file.read_file()
-        messages_file = FileManager("messages.json")
+        messages_file = FileManager("messages")
         messages = messages_file.read_file()
         send_message(users,channels,messages)
 
 def channel_entry():
-    writer=FileManager("channels.json")
+    writer=FileManager("channels")
     channel_url=input("Enter your Discord channel URL: ")
     channel_id=input("Enter your Discord channel ID: ")
     info=ChannelInfo(channel_url,channel_id)
     writer.write_file(info.to_dict())
 
 def user_entry():
-    writer=FileManager("users.json")
+    writer=FileManager("users")
     user_id = input("Enter your Discord user ID: ")
     user_token = input("Enter your Discord user token: ")
     info=UserInfo(user_id,user_token)
     writer.write_file(info.to_dict())
 
 def message_entry():
-    writer=FileManager("messages.json")
+    writer=FileManager("messages")
     message=input("Enter your message: ")
     info=MessageInfo(message)
     writer.write_file(info.to_dict())
